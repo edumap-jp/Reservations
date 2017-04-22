cce9fd4f45497ba71a9665027d568250b324057a
RyujiAMANO <ryuji@ryus.co.jp>
2017-04-22 13:49:46 +0900

    Add phpdoc comments.

--- Config/Migration/1492666075_add_reservation_location_permissions.php
+++ Config/Migration/1492666075_add_reservation_location_permissions.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * AddReservationLocationPermissions
+ *
+ * @author   Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+/**
+ * Class AddReservationLocationPermissions
+ */
 class AddReservationLocationPermissions extends CakeMigration {
 
 /**
