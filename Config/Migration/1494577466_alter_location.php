ce502ee8fe4b7fdb95a689746a155ea99a761480
s-nakajima <nakajimashouhei@gmail.com>
2017-05-21 23:37:45 +0900

    Installプラグインのテストでエラーになるため修正

--- Config/Migration/1494577466_alter_location.php
+++ Config/Migration/1494577466_alter_location.php
@@ -26,42 +26,16 @@ class AlterLocation extends CakeMigration {
  */
 	public $migration = array(
 		'up' => array(
-			'create_field' => array(
-				'reservation_events' => array(
-					'timezone' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'timezone_offset'),
-				),
-			),
 			'alter_field' => array(
 				'reservation_locations' => array(
 					'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 				),
 			),
-			'drop_table' => array(
-				'reservation_frame_setting_select_rooms'
-			),
 		),
 		'down' => array(
-			'drop_field' => array(
-				'reservation_events' => array('timezone'),
-			),
 			'alter_field' => array(
 				'reservation_locations' => array(
 					'modified' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
-				),
-			),
-			'create_table' => array(
-				'reservation_frame_setting_select_rooms' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'),
-					'reservation_frame_setting_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'comment' => '施設予約フレームセッティングのid'),
-					'room_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'comment' => 'ルームID'),
-					'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => '作成者'),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日時'),
-					'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => '更新者'),
-					'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日時'),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
 				),
 			),
 		),
