1b4272ce6dc9717751f7df1fab92f84fa7941d5d
RyujiAMANO <ryuji@ryus.co.jp>
2017-06-08 19:05:41 +0900

    コメント追加

--- Config/Migration/1496878521_alter_event_add_calendar_key_and_timezone.php
+++ Config/Migration/1496878521_alter_event_add_calendar_key_and_timezone.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * AlterEventAddCalendarKeyAndTimezone
+ *
+ * @author   Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+/**
+ * Class AlterEventAddCalendarKeyAndTimezone
+ */
 class AlterEventAddCalendarKeyAndTimezone extends CakeMigration {
 
 /**
